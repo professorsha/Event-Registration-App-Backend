55b0c0e709abb4b274015270e74bf8a7c26214de
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-09-21 13:53:34 +0300

    change routers

--- src/routers/users.js
+++ src/routers/users.js
@@ -7,5 +7,5 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 const router = Router();
 
 router.post('/users', ctrlWrapper(createUserController));
-router.get('/users/eventId', ctrlWrapper(getUserByIdController));
+router.get('/users/:eventId', ctrlWrapper(getUserByIdController));
 export default router;
